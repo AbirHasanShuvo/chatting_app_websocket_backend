cda785a6d11959dee72e71aa18318ab2b36c69ef
Md. Abir Hasan <abirhasanshuvo789@gmail.com>
2026-06-15 16:12:03 +0600

    completed the registration API

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,30 +4,38 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Storage;
 
 class UserController extends Controller
 {
     public function register(Request $request)
     {
-        // $request->validate([
-        //     'name' => 'required',
-        //     'date_of_birth' => 'required',
-        //     'email' => 'required|unique:users,email',
-        //     'phone' => 'required|unique:users,phone',
-        //     'password' => 'required',
-        //     'profile_picture' => 'required|image'
-        // ]);
+        $request->validate([
+            'name' => 'required',
+            'date_of_birth' => 'required',
+            'email' => 'required|unique:users,email',
+            'phone' => 'required|unique:users,phone',
+            'password' => 'required',
+            'profile_picture' => 'required|image'
+        ]);
 
         $file_path = $request->profile_picture->store('/profile_picture', 'public');
+        $profilePicture = Storage::url($file_path);
 
-        // User::create([
-        //     'name' => $request->name,
-        //     'date_of_birth' => $request->date_of_birth,
-        //     'email' => $request->email,
-        //     'phone' => $request->phone,
-        //     'password' => bcrypt($request->password)
-        // ]);
-        return $file_path;
+        $user = User::create([
+            'name' => $request->name,
+            'date_of_birth' => $request->date_of_birth,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'profile_picture' => $profilePicture,
+            'password' => bcrypt($request->password)
+        ]);
+
+        return response()->json([
+            'status' => 'Success',
+            'data' => $user
+        ]);
+
 
 
     }
